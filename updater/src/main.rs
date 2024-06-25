1a566263d44112085cb671012252bd3b094b0c07
Thibault Cheneviere <thibault.chene23@gmail.com>
2024-06-25 15:06:44 -0400

    feat: added db connection in rust script

--- updater/src/main.rs
+++ updater/src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello converter!");
+fn main() -> anyhow::Result<()> {
+    let url = "mysql://root:password@localhost:13306/f1db";
+    let _ = mysql::Pool::new(url)?;
+
+    Ok(())
 }
