128bb1e7cbf01c6f26744979804d86d264e22a35
Thibault Cheneviere <thibault.chene23@gmail.com>
2024-06-25 16:52:43 -0400

    feat: added table models

--- updater/src/main.rs
+++ updater/src/main.rs
@@ -1,3 +1,6 @@
+mod models;
+mod tables;
+
 fn main() -> anyhow::Result<()> {
     let url = "mysql://root:password@localhost:13306/f1db";
     let _ = mysql::Pool::new(url)?;
---
+++ updater/src/tables.rs
@@ -0,0 +1,104 @@
+use sea_query::Iden;
+
+#[derive(Iden)]
+pub enum ConstructorResults {
+    Table,
+    #[iden = "constructorResultId"]
+    ID,
+    #[iden = "raceId"]
+    RaceID,
+    #[iden = "constructorId"]
+    ConstructorID,
+    Points,
+    Status,
+}
+
+#[derive(Iden)]
+pub enum ConstructorStandings {
+    Table,
+    #[iden = "constructorStandingId"]
+    ID,
+    #[iden = "raceId"]
+    RaceID,
+    #[iden = "constructorId"]
+    ConstructorID,
+    Points,
+    Position,
+    #[iden = "positionText"]
+    PositionText,
+    Wins,
+}
+
+#[derive(Iden)]
+pub enum DriverStandings {
+    Table,
+    #[iden = "driverStandingId"]
+    ID,
+    #[iden = "raceId"]
+    RaceID,
+    #[iden = "driverId"]
+    DriverID,
+    Points,
+    Position,
+    #[iden = "positionText"]
+    PositionText,
+    Wins,
+}
+
+#[derive(Iden)]
+pub enum LapTimes {
+    Table,
+    #[iden = "raceId"]
+    RaceID,
+    #[iden = "driverId"]
+    DriverID,
+    Lap,
+    Position,
+    Time,
+    Milliseconds,
+}
+
+#[derive(Iden)]
+pub enum PitStops {
+    Table,
+    #[iden = "raceId"]
+    RaceID,
+    #[iden = "driverId"]
+    DriverID,
+    Stop,
+    Lap,
+    Time,
+    Duration,
+    Milliseconds,
+}
+
+#[derive(Iden)]
+pub enum Qualifying {
+    Table,
+    #[iden = "qualifyId"]
+    QualifyID,
+    #[iden = "raceId"]
+    RaceID,
+    #[iden = "driverId"]
+    DriverID,
+    #[iden = "constructorId"]
+    ConstructorID,
+    Number,
+    Position,
+    Q1,
+    Q2,
+    Q3,
+}
+
+#[derive(Iden)]
+pub enum Results {
+    Table,
+    #[iden = "resultId"]
+    ResultID,
+    #[iden = "raceId"]
+    RaceID,
+    #[iden = "driverId"]
+    DriverID,
+    #[iden = "constructorId"]
+    ConstructorID,
+}
